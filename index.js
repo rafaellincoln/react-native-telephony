f8453c4b9763634d6cfa17482b580081d5d2240d
Rafael Lincoln <rafaellincoln@enesolucoes.com.br>
2017-07-03 13:53:37 -0300

    addEventListener phoneState

--- index.js
+++ index.js
@@ -1,42 +1,27 @@
 /*
 * @providesModule react-native-telephony
 */
-import { NativeModules, NativeEventEmitter, Platform } from 'react-native'
+import { NativeModules } from 'react-native'
 const BatchedBridge = require('react-native/Libraries/BatchedBridge/BatchedBridge')
 
 // const NativeCallDetector = NativeModules.CallDetectionManager
 const PhoneState = NativeModules.PhoneState
 
-var TelephonyActionModule = require('./TelephonyActionModule')
+const TelephonyActionModule = require('./TelephonyActionModule')
 BatchedBridge.registerCallableModule('TelephonyActionModule', TelephonyActionModule)
 
-class CallDetectorManager {
+const PHONE_STATE = 'phoneState'
 
-    subscription;
-    callback
-    constructor(callback) {
-        this.callback = callback
-        if (Platform.OS === 'ios') {
-          return
-            // NativeCallDetector && NativeCallDetector.startListener()
-            // this.subscription = new NativeEventEmitter(NativeCallDetector)
-            // this.subscription.addListener('PhoneCallStateUpdate', callback);
-        }
-        else {
+export default class Telephony {
+    addEventListener(event, callback) {
+      switch (event) {
+        case PHONE_STATE:
             PhoneState && PhoneState.startListener()
-            TelephonyActionModule.callback = callback
-        }
-    }
-
-    dispose() {
-    	NativeCallDetector && NativeCallDetector.stopListener()
-    	NativeCallDetectorAndroid && NativeCallDetectorAndroid.stopListener()
-        CallStateUpdateActionModule.callback = undefined
-      if(this.subscription) {
-          this.subscription.removeAllListeners('PhoneCallStateUpdate');
-          this.subscription = undefined
+            TelephonyActionModule.callback = callback 
+          break;
+      
+        default:
+          break;
       }
     }
 }
-
-export default module.exports = CallDetectorManager;
